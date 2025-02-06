29337c8b0222970cf814f87ded1261f55e401360
david <david@abshift.com>
2025-02-06 12:55:10 +0400

    1

--- src/components/placeholder/Placeholder.tsx
+++ src/components/placeholder/Placeholder.tsx
@@ -14,7 +14,7 @@ export default function Placeholder() {
       </h1>
 
       <Image
-        src="/assets/logos/space/space_logo_tiny.png"
+        src="https://mymemecoin.com/wp-content/uploads/2025/01/Logo.svg"
         alt="MMC Logo"
         className="rotate-12 -mt-10 md:-mt-28"
         width={matches ? 400 : 150}
